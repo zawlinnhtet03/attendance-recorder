5db229ce23afdb76044a163edbb1d5515306e51e
Zaw Linn Htet <zawlinnhtet958@gmail.com>
2024-11-14 13:33:23 +0630

    changed

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    build: {
-        outDir: 'public/build',
-        manifest: true,
-    },
     plugins: [
         laravel({
             input: [
@@ -16,4 +12,3 @@ export default defineConfig({
         }),
     ],
 });
-
